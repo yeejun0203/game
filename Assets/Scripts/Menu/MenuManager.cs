57f014270b415c95ed790b4e2865d703e26f8a0a
HshMaker <man12297565@gmail.com>
2023-05-23 23:54:40 +0900

    ✨ 게임 메뉴 씬 추가 ✨

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    public void GameStart() => SceneManager.LoadScene("GameScene");
+}
