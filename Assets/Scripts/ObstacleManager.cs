0e9d985b913f405fd40b53a8946181a7442c1d4b
HshMaker <man12297565@gmail.com>
2023-06-02 22:23:26 +0900

    + 2 와 맵 확장

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -13,8 +13,8 @@ public class ObstacleManager : MonoBehaviour
 
     private void Start()
     {
-        int random = Random.Range(0, 6);
-        GenerateObstacle(0);
+        int random = Random.Range(0, 3);
+        GenerateObstacle(random);
     }
 
     void GenerateObstacle(int randomNum)
@@ -27,8 +27,14 @@ public class ObstacleManager : MonoBehaviour
                 obstacleC = Instantiate(obstacle[2], spawnPoints[Random.Range(6, 9)].position, transform.rotation);
                 break;
             case 1:
+                obstacleA = Instantiate(obstacle[2], spawnPoints[Random.Range(0, 3)].position, transform.rotation);
+                obstacleB = Instantiate(obstacle[2], spawnPoints[Random.Range(3, 6)].position, transform.rotation);
+                obstacleC = Instantiate(obstacle[2], spawnPoints[Random.Range(6, 9)].position, transform.rotation);
                 break;
             case 2:
+                obstacleA = Instantiate(obstacle[0], spawnPoints[FrontNum(0)].position, transform.rotation);
+                obstacleB = Instantiate(obstacle[1], spawnPoints[FrontNum(1)].position, transform.rotation);
+                obstacleC = Instantiate(obstacle[0], spawnPoints[FrontNum(2)].position, transform.rotation);
                 break;
             case 3:
                 break;
@@ -37,6 +43,26 @@ public class ObstacleManager : MonoBehaviour
             case 5:
                 break;
         }
+    }
+    int FrontNum(int i)
+    {
+        int num = 0;
+        if (i == 0)
+        {
+            int[] a = { 0, 3, 6 };
+            num = a[Random.Range(0, 3)];
+        }
+        else if (i == 1)
+        {
+            int[] a = { 1, 4, 7 };
+            num = a[Random.Range(0, 3)];
+        }
+        else if (i == 2)
+        {
+            int[] a = { 2, 5, 8 };
+            num = a[Random.Range(0, 3)];
+        }
+        return num;
     }
 
     private void OnDisable()
